f23367cdb9366903a198c4e230dd94bde247f621
Hilary Mallar <hilarymallar@gmail.com>
2019-01-26 17:13:56 -0500

    Updates

---
+++ precache-manifest.9a0c82164a964a0da9801fd0d66f2917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04517435260a9af4bf19",
+    "url": "/clickGame/static/js/runtime~main.04517435.js"
+  },
+  {
+    "revision": "1fbd07798a6ed7c87efe",
+    "url": "/clickGame/static/js/main.1fbd0779.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickGame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1fbd07798a6ed7c87efe",
+    "url": "/clickGame/static/css/main.edae6dcd.chunk.css"
+  },
+  {
+    "revision": "628da966caed39e2e5c8603005ba0c91",
+    "url": "/clickGame/index.html"
+  }
+];
